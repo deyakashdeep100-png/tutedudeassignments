d8414e8bbc280e7eec3384951b4f3ca88bc07ad7
deyakashdeep100-png <deyakashdeep100@gmail.com>
2026-02-21 16:01:41 +0530

    Assignment 1

---
+++ basicmathoperation.py
@@ -0,0 +1,7 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(f"Addition: {add}\nSubstraction: {sub}\nMultiplication: {mul}\nDivision: {div}")
---
+++ createapersonalizedgreeting.py
@@ -0,0 +1,6 @@
+fn=input("Enter your first name:")
+ln=input("Enter your last name:")
+print()
+print()
+print(f"Hello, {fn} {ln}! Welcome to the Python program")
+
